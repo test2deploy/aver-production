4128c08395e08d96263ad057de7ae8851f8329d6
Avni Alp Mumcu <117130162+test2deploy@users.noreply.github.com>
2026-06-01 17:42:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,77 @@
+// SMOOTH SCROLL
+
 document.querySelectorAll('a[href^="#"]').forEach((link) => {
+
   link.addEventListener("click", function (e) {
-    const target = document.querySelector(this.getAttribute("href"));
+
+    const target = document.querySelector(
+      this.getAttribute("href")
+    );
 
     if (target) {
+
       e.preventDefault();
+
       target.scrollIntoView({
         behavior: "smooth",
         block: "start"
       });
+
     }
+
   });
+
 });
+
+
+
+// LANGUAGE SYSTEM
+
+const trBtn = document.getElementById("trBtn");
+const enBtn = document.getElementById("enBtn");
+
+function setLanguage(lang) {
+
+  document.querySelectorAll(".translate").forEach((el) => {
+
+    el.innerHTML = el.getAttribute(`data-${lang}`);
+
+  });
+
+
+
+  document.querySelectorAll(".nav a").forEach((el) => {
+
+    el.innerHTML = el.getAttribute(`data-${lang}`);
+
+  });
+
+
+
+  localStorage.setItem("language", lang);
+
+}
+
+
+
+trBtn.addEventListener("click", () => {
+
+  setLanguage("tr");
+
+});
+
+
+
+enBtn.addEventListener("click", () => {
+
+  setLanguage("en");
+
+});
+
+
+
+// LOAD SAVED LANGUAGE
+
+const savedLang = localStorage.getItem("language") || "en";
+
+setLanguage(savedLang);
